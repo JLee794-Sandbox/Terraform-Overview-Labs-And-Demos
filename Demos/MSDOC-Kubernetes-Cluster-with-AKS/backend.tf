f4f1250a5b90a16be3781ce8c3326726d1b07a85
JinLee794 <94473824+JinLee794@users.noreply.github.com>
2022-09-08 09:36:45 -0500

    adding remote state

---
+++ Demos/MSDOC-Kubernetes-Cluster-with-AKS/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "bootstrap"
+    storage_account_name = "bootstrapsadev"
+    container_name       = "tfstate"
+    key                  = "msdoc-aks-demo.tfstate"
+  }
+}
